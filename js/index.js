5ec3fe366b62d10f24b43f098e6099f1292bca66
Shehzad Aslam Oza <shehzadaslamoza@gmail.com>
2022-02-11 21:56:39 +0500

    basic functionalities added

--- js/index.js
+++ js/index.js
@@ -1,6 +1,12 @@
 // global variables
 let container = document.querySelector(".container");
-let penColor = "#ff00ff";
+let clear = document.querySelector(".clear");
+let eraser = document.querySelector(".eraser");
+let color = document.getElementById("favcolor");
+let rainbow = document.querySelector(".rainbow");
+let colorMode = document.querySelector(".colorMode");
+let state = "C";
+let penColor = "#000000";
 
 // creates the n by n canvas for drawing
 function createDisplay(size) {
@@ -20,24 +26,18 @@ function createDisplay(size) {
     let el = document.createElement("div");
     el.classList.add("cell");
     el.classList.add("cell-" + i);
-    el.setAttribute("draggable", false);
     container.append(el);
   }
 }
 
-createDisplay(32);
+createDisplay(16);
 
 // Adds event listeners to each pixel
 let cellArray = document.querySelectorAll(".cell");
 console.log(cellArray);
 cellArray.forEach((element) => {
   element.addEventListener("mousedown", () => {
-    element.style.backgroundColor = penColor;
-    container.classList.add("dragging");
-  });
-
-  element.addEventListener("touchstart", () => {
-    element.style.backgroundColor = penColor;
+    element.style.backgroundColor = getColor(state);
     container.classList.add("dragging");
   });
 
@@ -45,19 +45,56 @@ cellArray.forEach((element) => {
     container.classList.remove("dragging");
   });
 
-  element.addEventListener("touchend", () => {
-    container.classList.remove("dragging");
-  });
-
   element.addEventListener("mouseover", () => {
     if (container.classList.contains("dragging")) {
-      element.style.backgroundColor = penColor;
-    }
-  });
-
-  element.addEventListener("touchmove", () => {
-    if (container.classList.contains("dragging")) {
-      element.style.backgroundColor = penColor;
+      element.style.backgroundColor = getColor(state);
     }
   });
 });
+
+// Adding event listners to the buttons
+clear.addEventListener("click", () => {
+  cellArray.forEach((element) => {
+    element.style.backgroundColor = "white";
+  });
+});
+
+eraser.addEventListener("click", () => {
+  state = "C";
+  penColor = "white";
+  rainbow.classList.remove("active");
+  colorMode.classList.remove("active");
+  eraser.classList.add("active");
+  color.value = "white";
+});
+
+color.addEventListener("change", () => {
+  penColor = color.value;
+  console.log(penColor);
+});
+
+rainbow.addEventListener("click", () => {
+  state = "R";
+  rainbow.classList.add("active");
+  colorMode.classList.remove("active");
+  eraser.classList.remove("active");
+});
+
+colorMode.addEventListener("click", () => {
+  state = "C";
+  colorMode.classList.add("active");
+  rainbow.classList.remove("active");
+  eraser.classList.remove("active");
+  penColor = "black";
+});
+
+function getColor(s) {
+  if (s == "C") {
+    return penColor;
+  } else {
+    let randomColor = Math.floor(Math.random() * 16777215).toString(16);
+    penColor = "#" + randomColor;
+    color.value = penColor;
+    return penColor;
+  }
+}
